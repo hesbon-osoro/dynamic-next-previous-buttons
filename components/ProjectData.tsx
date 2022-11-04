9ac3119585046c7be7b35cc99eeb19cec2159879
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-11-05 00:19:07 +0300

    main bug fixes

--- components/ProjectData.tsx
+++ components/ProjectData.tsx
@@ -1,6 +1,6 @@
-//@ts-nocheck
+// @ts-nocheck
 import React, { FC, useEffect, useState } from 'react';
-import { data /* DataProp */ } from '../data';
+import { data, DataProp } from '../data';
 import { Modal, Project } from '.';
 import styles from '../styles/projectData.module.css';
 
@@ -13,21 +13,22 @@ import styles from '../styles/projectData.module.css';
 
 const ProjectData: FC = () => {
 	const [state, setState] = useState({
-		projects: [],
+		// projects: [],
 		activeProject: '',
 		modalTitle: '',
 		modalDescription: '',
 	});
+	const [projects, setProjects] = useState<DataProp[]>([]);
 
 	useEffect(() => {
-		setState({ projects: data });
+		setProjects(data);
 	}, []);
 
 	const handleModelOpen = idx => {
 		setState({
 			activeProject: idx,
-			modalTitle: state.projects[idx].title,
-			modalDescription: state.projects[idx].description,
+			modalTitle: projects[idx].title,
+			modalDescription: projects[idx].description,
 		});
 	};
 
@@ -36,32 +37,32 @@ const ProjectData: FC = () => {
 	};
 
 	const handleNextProject = () => {
-		let len = state.projects.length;
+		let len = projects.length;
 		let idx = state.activeProject + 1;
 		idx = idx % len;
 
 		setState({
 			activeProject: idx,
-			modalTitle: state.projects[idx].title,
-			modalDescription: state.projects[idx].description,
+			modalTitle: projects[idx].title,
+			modalDescription: projects[idx].description,
 		});
 	};
 
 	const handlePrevProject = () => {
-		let len = state.projects.length;
-		let idx = parseInt(state.activeProject) - 1;
-		idx = idx % len;
+		let len = projects.length;
+		let idx = state.activeProject;
+		// idx = idx % len;
 
-		// if (idx === 0) {
-		// 	idx = length - 1;
-		// } else {
-		// 	idx = idx - 1;
-		// }
+		if (idx === 0) {
+			idx = len - 1;
+		} else {
+			idx = idx - 1;
+		}
 
 		setState({
 			activeProject: idx,
-			modalTitle: state.projects[idx].title,
-			modalDescription: state.projects[idx].description,
+			modalTitle: projects[idx].title,
+			modalDescription: projects[idx].description,
 		});
 	};
 	function nextTitle(idx, arr) {
@@ -71,7 +72,7 @@ const ProjectData: FC = () => {
 	}
 
 	function prevTitle(idx, arr) {
-		let i;
+		let i = 0;
 		if (idx === 0) {
 			i = arr.length - 1;
 		} else {
@@ -84,33 +85,34 @@ const ProjectData: FC = () => {
 		<Project
 			key={`project-${arr.id}`}
 			index={idx}
+			title={arr.title}
 			url={arr.url}
 			onModalOpen={handleModelOpen}
 		/>
 	));
 
-	if (!state.activeProject) {
+	if (state.activeProject === '') {
 		return (
 			<div className={styles.projectComponents}>
 				<h3>Click on a project below to view details</h3>
 				<div className={styles.projects}>{projectComponents}</div>
 			</div>
 		);
+	} else {
+		return (
+			<div>
+				<Modal
+					title={state.modalTitle}
+					description={state.modalDescription}
+					previousTitle={prevTitle(state.activeProject, data)}
+					nextTitle={nextTitle(state.activeProject, data)}
+					onModalClose={handleModalClose}
+					onNext={handleNextProject}
+					onPrev={handlePrevProject}
+				/>
+			</div>
+		);
 	}
-
-	return (
-		<div>
-			<Modal
-				title={state.modalTitle}
-				description={state.modalDescription}
-				previousTitle={prevTitle(state.activeProject, data)}
-				nextTitle={nextTitle(state.activeProject, data)}
-				onModalClose={handleModalClose}
-				onNext={handleNextProject}
-				onPrev={handlePrevProject}
-			/>
-		</div>
-	);
 };
 
 export default ProjectData;
